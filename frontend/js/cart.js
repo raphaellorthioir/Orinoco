cc179047849a6dfd16dbc1d610802e8e2bbe128a
raphael.lorthioir <raphael.lorthioir@gmail.com>
2021-11-24 16:17:37 +0100

    responsive complete

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -60,68 +60,66 @@ import {callApi, url} from "./modules/callApi.js"
       for(let i=0; i<ls.length; i++){
         if(camera._id === ls[i].id ){
 
-          let tbody= document.querySelector("#createLine")
-          let tr = document.createElement("tr")
-          tbody.appendChild(tr)
-          let tdDescription = document.createElement("td")
-          tdDescription.setAttribute("class","align center")
-          tr.appendChild(tdDescription)
-          let divNameOpt = document.createElement("div")
-          divNameOpt.setAttribute("class","col")
-          let lienPageProduit = document.createElement("a")
-          lienPageProduit.setAttribute("href",`product.html?id=${camera._id}`)
-          tdDescription.appendChild(lienPageProduit)
-          tdDescription.appendChild(divNameOpt)
-          let imgProduit = document.createElement("img")
-          imgProduit.setAttribute("src",`${camera.imageUrl}`)
-          lienPageProduit.appendChild(imgProduit)
-          let nameModel = document.createElement("p")
-          nameModel.textContent =` ${ls[i].name}`
-          let nameOption = document.createElement("p") 
-          divNameOpt.appendChild(nameModel)
-          divNameOpt.appendChild(nameOption)
-          nameOption.textContent=`Lentille choisie: ${ls[i].option}`
+          let ul = document.querySelector("#listProduct")
+          let li = document.createElement("li")
+          li.setAttribute("class"," align")
+          li.setAttribute("id","list")
+          ul.appendChild(li)
+          let divImg = document.createElement("div")
+          divImg.setAttribute("class","center")
+          let divInfo = document.createElement("div")
+          divInfo.setAttribute("class","col center")
+          li.appendChild(divImg)
+          li.appendChild(divInfo)
+          let img = document.createElement("img")
+          img.setAttribute("src",`${camera.imageUrl}`)
+          img.setAttribute("width","300px")
+          divImg.appendChild(img)
          
-          let tdPrice = document.createElement("td")
-          tr.appendChild(tdPrice)
-          tdPrice.textContent=`${ls[i].prix}€`
+          let nameProduct = document.createElement("p")
+          let priceProduct = document.createElement("p")
+          let productQty = document.createElement("div")
+          productQty.setAttribute("class","align")
+          let option = document.createElement("p")
+          let totalProduct = document.createElement("p")
+          divInfo.appendChild(nameProduct)
+          divInfo.appendChild(priceProduct)
+          divInfo.appendChild(productQty)
+          divInfo.appendChild(option)
+          divInfo.appendChild(totalProduct)
+          nameProduct.textContent =` ${ls[i].name}`
+          option.textContent=`Lentille choisie: ${ls[i].option}`
+          priceProduct.textContent=`Prix: ${ls[i].prix}€`
 
-          let tdQty = document.createElement("td")
-          tr.appendChild(tdQty)
-          let divBtnQty = document.createElement("div")
-          divBtnQty.setAttribute("class","align center")
-          tdQty.appendChild(divBtnQty)
           let lessBtn = document.createElement("button")
           lessBtn.setAttribute("class","howMuchBtn")
-          divBtnQty.appendChild(lessBtn)
+          productQty.appendChild(lessBtn)
           lessBtn.textContent="-"
           let divCurrentQty = document.createElement("div")
           divCurrentQty.setAttribute("class","chooseNumber")
           divCurrentQty.setAttribute("id","qty")
           divCurrentQty.textContent=`${ls[i].quantite}`
-          divBtnQty.appendChild(divCurrentQty)
+          productQty.appendChild(divCurrentQty)
           let plusBtn = document.createElement("button")
           plusBtn.textContent="+"
           plusBtn.setAttribute("class","howMuchBtn")
-          divBtnQty.appendChild(plusBtn)
+          productQty.appendChild(plusBtn)
 
+         
+        //  let divPriceCancel =document.createElement("div")
+         // textTotal.appendChild(divPriceCancel)
+         // divPriceCancel.setAttribute("class"," align center space  ")
+         // divPriceCancel.setAttribute("id","divCancelProduct")
           
-          let textTotal= document.createElement("td")
-          tr.appendChild(textTotal)
-          let divPriceCancel =document.createElement("div")
-          textTotal.appendChild(divPriceCancel)
-          divPriceCancel.setAttribute("class"," align center space-b ")
-          divPriceCancel.setAttribute("id","divCancelProduct")
-          let totalPrice = document.createElement("p")
-          divPriceCancel.appendChild(totalPrice)
+          //divPriceCancel.appendChild(totalPrice)
           let  total= ls[i].quantite * ls[i].prix
-          totalPrice.textContent=`${total} €`
+          totalProduct.textContent=`Total:${total} €`
           let cancelBtn = document.createElement("button")
           cancelBtn.setAttribute("id","cancelProduct")
-          let cancelBtnIcon = document.createElement("i")
-          cancelBtnIcon.setAttribute("class","fas fa-times")
-          cancelBtn.appendChild(cancelBtnIcon)
-          divPriceCancel.appendChild(cancelBtn)
+         // let cancelBtnIcon = document.createElement("i")
+          //cancelBtnIcon.setAttribute("class","fas fa-times")
+         // cancelBtn.appendChild(cancelBtnIcon)
+         // textTotal.appendChild(cancelBtn)
          
 
           lessBtn.addEventListener("click", ()=>{
@@ -131,7 +129,7 @@ import {callApi, url} from "./modules/callApi.js"
               localStorage.setItem("produit",JSON.stringify(ls))
               divCurrentQty.textContent=`${ls[i].quantite}`
               total = total - ls[i].prix
-              totalPrice.textContent=`${total}€`
+              totalProduct.textContent=`Total: ${total}€`
               }
               getAllTotal();
          });
@@ -142,7 +140,7 @@ import {callApi, url} from "./modules/callApi.js"
             localStorage.setItem("produit",JSON.stringify(ls))
             divCurrentQty.textContent=`${ls[i].quantite}`
             total = total + ls[i].prix
-            totalPrice.textContent=`${total}€`
+            totalProduct.textContent=`Total:${total}€`
             }
             getAllTotal();
          });
@@ -184,7 +182,7 @@ userForm.addEventListener('submit',(e)=>{
    contact,
    products,
 }
-  
+
 
   
   contact.firstName = document.querySelector("#firstName").value 
@@ -193,8 +191,12 @@ userForm.addEventListener('submit',(e)=>{
   contact.city = document.querySelector("#city").value
   contact.email = document.querySelector("#userMail").value
  
-  
-  
+  for(let i=0; i<ls.length;i++){
+    for(let j=0; j<ls[i].quantite;j++){
+      products.push(ls[i].id)
+    }
+  }
+  console.log(newUser)
  const urlOrder = fetch("http://localhost:3000/api/cameras/order",{
 
     method: "POST",
@@ -206,9 +208,9 @@ userForm.addEventListener('submit',(e)=>{
 
 urlOrder.then(response => response.json()).then( (response)=>{
   try{
-    
+     
     response.products=ls
-    getAllTotal()
+   getAllTotal()
     response.totalOrder = totalCart
     console.log(totalCart)
     localStorage.setItem("order",JSON.stringify(response))
